6ce5a44fad9417c7ec14b758bbc27c7672db49a2
Paul Marcelis <paul.marcelis@kpn.com>
2018-06-26 15:10:34 +0200

    extend payload encryption function to support downlink payload encryption as well

--- payload-decryption/PayloadEncryption.php
+++ payload-decryption/PayloadEncryption.php
@@ -13,6 +13,8 @@
  * Class to do encryption/decryption of LoRaWAN Payload. Since AES-128 is a symmetric algorithm, the encryption function can also be used for decryption.
  */
 
+ require_once(__DIR__ . "/Helper.php");
+
 class PayloadEncryption {
 
   const DIR_UPLINK = 0;
@@ -33,16 +35,24 @@ class PayloadEncryption {
     $this->_appSKey = $appSKey;
   }
 
+  public function uplink($payloadHexString, $devAddr, $fCntUp) {
+    return $this->payload($payloadHexString, static::DIR_UPLINK, $devAddr, $fCntUp);
+  }
+  
+  public function downlink($payloadHexString, $devAddr, $fCntDown) {
+    return $this->payload($payloadHexString, static::DIR_DOWNLINK, $devAddr, $fCntDown);
+  }
+
   /**
    * 
    * @param string $payloadHexString - case insensitive
    * @param int $dir - DIR_UPLINK or DIR_DOWNLINK
    * @param string $devAddr - case insensitive
-   * @param int $fCntUp
+   * @param int $fCnt - fCntUp for uplink, fCntDown for downlink
    * @return string - the encrypted/decrypted payload
    * @throws Exception
    */
-  public function payload($payloadHexString, $dir, $devAddr, $fCntUp) {
+  public function payload($payloadHexString, $dir, $devAddr, $fCnt) {
     // check payload length
     if ((strlen($payloadHexString) % 2) !== 0 || strlen($payloadHexString) === 0) {
       throw new Exception("Payload has invalid length. Should be an even number of hex characters");
@@ -63,7 +73,7 @@ class PayloadEncryption {
     $payloadHexStringBlocks = str_split($payloadHexString, $blockSize * 2);
     $payloadEncryptedHexString = "";
     for ($i = 0; $i < $nrBlocks; $i++) {
-      $blockEncryptedHexString = $this->payloadBlock($payloadHexStringBlocks[$i], $dir, $devAddr, $fCntUp, $i + 1);
+      $blockEncryptedHexString = $this->payloadBlock($payloadHexStringBlocks[$i], $dir, $devAddr, $fCnt, $i + 1);
       $payloadEncryptedHexString .= $blockEncryptedHexString;
     }
     return substr($payloadEncryptedHexString, 0, $payloadSize * 2);
@@ -74,29 +84,29 @@ class PayloadEncryption {
    * @param string $blockHexString
    * @param int $dir
    * @param string $devAddr
-   * @param int $fCntUp
+   * @param int $fCnt
    * @param int $blockNr 1-based index number of the block to be encrypted/decrypted
    * @return string
    */
-  public function payloadBlock($blockHexString, $dir, $devAddr, $fCntUp, $blockNr) {
+  public function payloadBlock($blockHexString, $dir, $devAddr, $fCnt, $blockNr) {
     if (preg_match(static::REGEX_16BYTES, $blockHexString) !== 1) {
       throw new Exception("Block is incorrect. Should be 16 bytes in HEX representation.");
     } else if (preg_match(static::REGEX_4BYTES, $devAddr) !== 1) {
       throw new Exception("DevAddr is incorrect. Should be 4 bytes in HEX representation.");
-    } else if (!is_int($fCntUp)) {
-      throw new Exception("FCntUp should be an integer.");
+    } else if (!is_int($fCnt)) {
+      throw new Exception("FCnt should be an integer.");
     }
     $devAddrByteArray = Helper::hexStringtoByteArray($devAddr, 4);
-    $fCntUpByteArray = Helper::integerToByteArray($fCntUp, 4);
+    $fCntByteArray = Helper::integerToByteArray($fCnt, 4);
     $blockNrByteArray = Helper::integerToByteArray($blockNr, 1);
 
-    // prepare block A_i as described in LW102 Page 19, Line 12. Put DevAddr and FCntUp Big Endian in.
+    // prepare block A_i as described in LW102 Page 19, Line 12. Put DevAddr and FCnt Big Endian in.
     $a = [
       0x01,
       0x00, 0x00, 0x00, 0x00,
       $dir,
       $devAddrByteArray[3], $devAddrByteArray[2], $devAddrByteArray[1], $devAddrByteArray[0],
-      $fCntUpByteArray[3], $fCntUpByteArray[2], $fCntUpByteArray[1], $fCntUpByteArray[0],
+      $fCntByteArray[3], $fCntByteArray[2], $fCntByteArray[1], $fCntByteArray[0],
       0x00,
       $blockNrByteArray[0]
     ];
